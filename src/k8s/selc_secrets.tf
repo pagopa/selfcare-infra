4bdc4ffdbb63d2ee05beb29ba5de25f8912c95d0
anttorre <antonio.torre@everis.com>
2021-10-06 16:55:18 +0200

    redis-credential settings

--- src/k8s/selc_secrets.tf
+++ src/k8s/selc_secrets.tf
@@ -11,6 +11,21 @@ resource "kubernetes_secret" "hub-spid-login-ms" {
   type = "Opaque"
 }
 
+resource "kubernetes_secret" "selc-redis-credentials" {
+  metadata {
+    name      = "redis-credentials"
+    namespace = kubernetes_namespace.selc.metadata[0].name
+  }
+
+  data = {
+    REDIS_URL=local.redis_url
+    REDIS_PASSWORD=module.key_vault_secrets_query.values["redis-primary-access-key"].value
+    REDIS_PORT="6379"
+  }
+
+  type = "Opaque"
+}
+
 # not yet used by any deployment, but maybe useful for the future
 resource "kubernetes_secret" "selc-application-insights" {
   metadata {
