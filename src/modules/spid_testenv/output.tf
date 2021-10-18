7f907c01fb32515566440e9ebee51ef093521854
anttorre <antonio.torre@everis.com>
2021-10-18 09:50:53 +0200

    spid-testenv output var not null only if enabled

--- src/modules/spid_testenv/output.tf
+++ src/modules/spid_testenv/output.tf
@@ -1,9 +1,9 @@
 output "container_id" {
   description = "The id of the spid_testenv container."
-  value       = azurerm_container_group.spid_testenv[0].id
+  value       = var.enable_spid_test? azurerm_container_group.spid_testenv[0].id : null
 }
 
 output "spid_testenv_url" {
   description = "The id of the spid_testenv container."
-  value       = azurerm_container_group.spid_testenv[0].fqdn
+  value       = var.enable_spid_test? azurerm_container_group.spid_testenv[0].fqdn : null
 }
