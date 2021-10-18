de035cda55cff534f3b265f95573e1827e35531b
uolter <walter.traspadini@pagopa.it>
2021-10-18 18:32:44 +0200

    removed vnet integration and its peering

--- src/core/dns_private.tf
+++ src/core/dns_private.tf
@@ -15,16 +15,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_documents_
   tags = var.tags
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_documents_azure_com_vnet_integration" {
-  name                  = module.vnet_integration.name
-  resource_group_name   = azurerm_resource_group.rg_vnet.name
-  private_dns_zone_name = azurerm_private_dns_zone.privatelink_documents_azure_com.name
-  virtual_network_id    = module.vnet_integration.id
-  registration_enabled  = false
-
-  tags = var.tags
-}
-
 resource "azurerm_private_dns_zone" "privatelink_mongo_cosmos_azure_com" {
   name                = "privatelink.mongo.cosmos.azure.com"
   resource_group_name = azurerm_resource_group.rg_vnet.name
@@ -37,16 +27,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_mongo_cosm
   resource_group_name   = azurerm_resource_group.rg_vnet.name
   private_dns_zone_name = azurerm_private_dns_zone.privatelink_mongo_cosmos_azure_com.name
   virtual_network_id    = module.vnet.id
-  registration_enabled  = false
-
-  tags = var.tags
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_mongo_cosmos_azure_com_vnet_integration" {
-  name                  = module.vnet_integration.name
-  resource_group_name   = azurerm_resource_group.rg_vnet.name
-  private_dns_zone_name = azurerm_private_dns_zone.privatelink_mongo_cosmos_azure_com.name
-  virtual_network_id    = module.vnet_integration.id
   registration_enabled  = false
 
   tags = var.tags
