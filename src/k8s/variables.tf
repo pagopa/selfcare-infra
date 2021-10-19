7add28d2a840a504d058db7849a428c3c49ebe96
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-10-19 16:10:35 +0200

    K8s (#18)
    selc namespace + ingress
    
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ src/k8s/variables.tf
@@ -0,0 +1,56 @@
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "prefix" {
+  type    = string
+  default = "selc"
+}
+
+variable "env" {
+  type = string
+}
+
+variable "env_short" {
+  type = string
+}
+
+variable "k8s_kube_config_path" {
+  type    = string
+  default = "~/.kube/config"
+}
+
+variable "k8s_apiserver_host" {
+  type = string
+}
+
+variable "k8s_apiserver_port" {
+  type    = number
+  default = 443
+}
+
+variable "k8s_apiserver_insecure" {
+  type    = bool
+  default = false
+}
+
+variable "rbac_namespaces" {
+  type    = list(string)
+  default = ["selc"]
+}
+
+# ingress
+
+variable "ingress_replica_count" {
+  type = string
+}
+
+variable "ingress_load_balancer_ip" {
+  type = string
+}
+
+variable "default_service_port" {
+  type    = number
+  default = 8080
+}
