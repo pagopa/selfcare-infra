0eddd3007133baa234cf3078c976eeab026bd433
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-10-20 13:26:06 +0200

    Base secrets configs (#23)
    * removed portal and management from apim
    
    * cleaned dev vars
    
    * fixed certificate reference from keyvault
    
    * precommit run
    
    * aks-infra
    
    * precommit run
    
    * setting maxPods
    
    * defining alert metrics
    
    * aks_node_count to 1 in PROD temporarily
    
    * max_pods defined as variable
    
    * k8s base
    
    * base secrets and configs
    
    * hubSpidLogin config
    
    * hubSpid login configMap and secrets
    
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ src/k8s/selc_secrets.tf
@@ -0,0 +1,46 @@
+resource "kubernetes_secret" "hub-spid-login-ms" {
+  metadata {
+    name      = "hub-spid-login-ms"
+    namespace = kubernetes_namespace.selc.metadata[0].name
+  }
+
+  data = {
+    APPINSIGHTS_INSTRUMENTATIONKEY = local.appinsights_instrumentation_key
+    JWT_TOKEN_PRIVATE_KEY          = module.key_vault_secrets_query.values["jwt-private-key"].value
+
+    METADATA_PUBLIC_CERT  = module.key_vault_secrets_query.values["agid-spid-cert"]        # TODO actually manually populated, but to try to automate in gitops
+    METADATA_PRIVATE_CERT = module.key_vault_secrets_query.values["agid-spid-private-key"] # TODO actually manually populated, but to try to automate in gitops
+
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret" "selc-redis-credentials" {
+  metadata {
+    name      = "redis-credentials"
+    namespace = kubernetes_namespace.selc.metadata[0].name
+  }
+
+  data = {
+    REDIS_URL      = local.redis_url
+    REDIS_PASSWORD = module.key_vault_secrets_query.values["redis-primary-access-key"].value
+    REDIS_PORT     = "6379"
+  }
+
+  type = "Opaque"
+}
+
+# not yet used by any deployment, but maybe useful for the future
+resource "kubernetes_secret" "selc-application-insights" {
+  metadata {
+    name      = "application-insights"
+    namespace = kubernetes_namespace.selc.metadata[0].name
+  }
+
+  data = {
+    APPLICATIONINSIGHTS_CONNECTION_STRING = local.appinsights_instrumentation_key
+  }
+
+  type = "Opaque"
+}
--- src/k8s/subscriptions/UAT-SelfCare/terraform.tfvars
+++ src/k8s/subscriptions/UAT-SelfCare/terraform.tfvars
@@ -4,3 +4,17 @@ env_short = "u"
 # ingress
 ingress_replica_count    = "2"
 ingress_load_balancer_ip = "10.1.0.250"
+
+# Gateway
+api_gateway_url  = "https://api.uat.selfcare.pagopa.it"
+cdn_frontend_url = "https://uat.selfcare.pagopa.it"
+
+
+# hub-spid-login-ms
+configmaps_hub-spid-login-ms = {
+  APPLICATIONINSIGHTS_ROLE_NAME                     = "hub-spid-login-ms"
+  APPLICATIONINSIGHTS_INSTRUMENTATION_LOGGING_LEVEL = "OFF"
+
+  # SPID
+  ORG_ISSUER = "https://uat.selfcare.pagopa.it" # TODO to verify
+}
