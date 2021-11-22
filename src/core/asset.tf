af1210cd4de1967441bb4949d7ac5e68cb03de4c
anttorre <antonio.torre@everis.com>
2021-11-22 21:41:14 +0100

    using blob sync to upload entire asset folder

--- src/core/asset.tf
+++ src/core/asset.tf
@@ -1,49 +1,29 @@
-## Terms and Conditions
 data "local_file" "tc" {
   filename = "${path.module}/assets/tc.pdf"
 }
-resource "null_resource" "upload_tc" {
-  triggers = {
-    "changes-in-config" : md5(data.local_file.tc.content)
-  }
-  provisioner "local-exec" {
-    command = <<EOT
-              az config set extension.use_dynamic_install=yes_without_prompt
-              az storage azcopy blob upload \
-                 --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
-                 --account-key ${module.checkout_cdn.storage_primary_access_key} \
-                 --container '$web' \
-                 --destination 'assets/' \
-                 --source "${path.module}/assets/tc.pdf"
-              az cdn endpoint purge -g ${azurerm_resource_group.checkout_fe_rg.name} -n ${module.checkout_cdn.name} \
-                 --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")} \
-                 --content-paths "/assets/tc.pdf" \
-                 --no-wait
-          EOT
-  }
-}
-
-## Informativa Privacy
 data "local_file" "informativa-privacy" {
   filename = "${path.module}/assets/InformativaPrivacy.pdf"
 }
-resource "null_resource" "upload_informativa-privacy" {
+
+
+resource "null_resource" "upload_tc" {
   triggers = {
-    "changes-in-config" : md5(data.local_file.informativa-privacy.content)
+    "changes-in-tc" : md5(data.local_file.tc.content)
+    "changes-in-informativa-privacy" : md5(data.local_file.informativa-privacy.content)
   }
   provisioner "local-exec" {
     command = <<EOT
-              az config set extension.use_dynamic_install=yes_without_prompt
-              az storage azcopy blob upload \
-                 --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
-                 --account-key ${module.checkout_cdn.storage_primary_access_key} \
-                 --container '$web' \
-                 --destination 'assets/' \
-                 --source "${path.module}/assets/InformativaPrivacy.pdf"
-              az cdn endpoint purge -g ${azurerm_resource_group.checkout_fe_rg.name} -n ${module.checkout_cdn.name} \
-                 --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")} \
-                 --content-paths "/assets/InformativaPrivacy.pdf" \
-                 --no-wait
+              az storage blob sync \
+                --container '$web' \
+                --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
+                --account-key ${module.checkout_cdn.storage_primary_access_key} -s "./assets" \
+                --destination 'assets/'
+              az cdn endpoint purge \
+                -g ${azurerm_resource_group.checkout_fe_rg.name} \
+                -n ${module.checkout_cdn.name} \
+                --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")}  \
+                --content-paths "/assets/*" \
+                --no-wait
           EOT
   }
 }
