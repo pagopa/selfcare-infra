ba7943542c1c9ebcdc85d2923f8f69b0b84d0686
anttorre <antonio.torre@everis.com>
2021-11-30 14:25:13 +0100

    oldFile not considered if empty

--- src/core/utils/py/jwksFromPems.py
+++ src/core/utils/py/jwksFromPems.py
@@ -41,8 +41,9 @@ old_jwks = None
 keys = []
 if exists(sys.argv[1]) :
     old_jwks_data = read_file(sys.argv[1])
-    old_jwks = JsonWebKey.import_key_set(old_jwks_data)
-    keys.append(re.sub("\s*]\s*}", "", re.sub('^\s*\{\s*"keys"\s*:\s*\[\s*', "", old_jwks_data)))
+    if old_jwks_data != "" :
+        old_jwks = JsonWebKey.import_key_set(old_jwks_data)
+        keys.append(re.sub("\s*]\s*}", "", re.sub('^\s*\{\s*"keys"\s*:\s*\[\s*', "", old_jwks_data)))
 
 for i in range(2, len(sys.argv)):
     pem = sys.argv[i]
