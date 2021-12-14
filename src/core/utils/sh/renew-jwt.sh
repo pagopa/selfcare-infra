428ba753f4950fd6d1ebab50c5c234b1b5002060
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-12-14 08:25:39 +0100

    agid cert is a self signed cert (#83)
    renew jwt token script
    
    Co-authored-by: anttorre <antonio.torre@everis.com>

---
+++ src/core/utils/sh/renew-jwt.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+# Renew the JWT token inside the keyvault. After this script you have to be connected to the env VPN and apply the k8s
+# module in order to update configMap and secrets and next run the k8s/scripts/restart-pods.sh in order to apply
+# the new configuration
+#
+# Environments are dev, uat or prod
+# Usage:
+#  ./renew-jwt.sh env jwt_name
+#
+#  ./renew-jwt.sh dev jwt
+#  ./renew-jwt.sh dev jwt_exchange
+#  ./renew-jwt.sh dev agid_spid
+
+set -e
+
+BASEDIR=$(dirname "$0")
+
+env=$1
+jwt_name=$2
+
+if [ -z "$env" ]; then
+  echo "env should be: dev, uat or prod."
+  exit 0
+fi
+
+if [ -z "$jwt_name" ]; then
+  echo "jwt_name should be: jwt, jwt_exchange or agid_spid."
+  exit 0
+fi
+
+"$BASEDIR"/../../terraform.sh taint $env module.$jwt_name.tls_private_key.jwt
+"$BASEDIR"/../../terraform.sh apply $env -target=module.$jwt_name
+
+printf "\n\n************************************************************************************************\n\n"
+echo "Now you have to be connected to env VPN in order to apply k8s module and next run k8s/scripts/restart-pods.sh script"
+printf "\n************************************************************************************************"
