7e7d53366dc0653269ddaef34f9ad5f7ae5fa5e7
anttorre <antonio.torre@everis.com>
2022-03-01 17:00:37 +0100

    sync hìjust resources templates folder

--- src/core/resources-templates.tf
+++ src/core/resources-templates.tf
@@ -1,19 +1,18 @@
 resource "null_resource" "upload_resources" {
   triggers = {
-    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/resources", "**") : filesha1("${path.module}/resources/${f}")]))
+    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/resources_templates", "**") : filesha1("${path.module}/resources_templates/${f}")]))
   }
   provisioner "local-exec" {
     command = <<EOT
-              az storage blob sync \
-                --container '$web' \
+              az storage blob sync --container '$web' \
                 --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
-                --account-key ${module.checkout_cdn.storage_primary_access_key} -s "./resources" \
-                --destination 'resources/'
+                --account-key ${module.checkout_cdn.storage_primary_access_key} -s "./resources_templates" \
+                --destination 'resources/templates/'
               az cdn endpoint purge \
                 -g ${azurerm_resource_group.checkout_fe_rg.name} \
                 -n ${module.checkout_cdn.name} \
                 --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")}  \
-                --content-paths "/resources/*" \
+                --content-paths "/resources/templates/*" \
                 --no-wait
           EOT
   }
