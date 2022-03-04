1eb73bcd9a16117c09b44a4989f985a31b3d9f1a
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-03-04 12:41:39 +0100

    SELC-472 secret cdn-storage + ms-product config (#124)
    Co-authored-by: anttorre <antonio.torre@everis.com>
    Co-authored-by: Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>

--- src/k8s/selc_configmaps.tf
+++ src/k8s/selc_configmaps.tf
@@ -104,6 +104,7 @@ resource "kubernetes_config_map" "ms-product" {
   data = merge({
     JWT_TOKEN_PUBLIC_KEY = module.key_vault_secrets_query.values["jwt-public-key"].value
     MONGODB_NAME         = local.mongodb_name_selc_product
+    LOGO_STORAGE_URL     = format("%s/resources/products/default/logo.png", var.cdn_storage_url)
     },
     var.configmaps_ms-product
   )
