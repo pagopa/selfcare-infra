dcacede99651869248fb307b1076fc747b5faff0
anttorre <antonio.torre@everis.com>
2022-05-13 16:51:59 +0200

    fixing GRANT on party projections

--- src/psql/migrations/selc/V3__party_projection.sql
+++ src/psql/migrations/selc/V3__party_projection.sql
@@ -10,6 +10,8 @@ CREATE TABLE IF NOT EXISTS party.akka_projection_offset_store (
 
 CREATE INDEX IF NOT EXISTS projection_name_index ON party.akka_projection_offset_store (projection_name);
 
+ALTER TABLE party.akka_projection_offset_store OWNER TO "PARTY_USER";
+
 CREATE TABLE IF NOT EXISTS party.akka_projection_management (
   projection_name VARCHAR(255) NOT NULL,
   projection_key VARCHAR(255) NOT NULL,
@@ -17,3 +19,5 @@ CREATE TABLE IF NOT EXISTS party.akka_projection_management (
   last_updated BIGINT NOT NULL,
   PRIMARY KEY(projection_name, projection_key)
 );
+
+ALTER TABLE party.akka_projection_management OWNER TO "PARTY_USER";
