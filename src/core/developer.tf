42bfe7434573cc32ae750fc9c0c02328fd3aa911
Andrea Putzu <andrea.putzu@pagopa.it>
2022-09-05 19:20:39 +0200

    update trigger

--- src/core/developer.tf
+++ src/core/developer.tf
@@ -5,7 +5,7 @@ locals {
   azure_apim_api_version = "2021-08-01"
 }
 
-resource "null_resource" "read_apim_api" {
+resource "null_resource" "download_apim_external_api_v1" {
   triggers = {
     #build_number = "${timestamp()}"
     dir_sha1 = sha1(join("", [for f in fileset("./api/ms_external_api", "**") : filesha1("./api/ms_external_api/${f}")]))
@@ -19,17 +19,17 @@ resource "null_resource" "read_apim_api" {
       az rest \
         --method get \
         --url "https://management.azure.com/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${local.resource_groups_name}/providers/Microsoft.ApiManagement/service/${local.service_name}/apis/${local.api_id}?api-version=${local.azure_apim_api_version}&export=true&format=openapi" \
-        --output-file ./env/${var.env}/openapi/ms-external-api-v1.yaml
+        --output-file ./env/${var.env}/developer/external/ms-external-api-v1.yaml
     EOT
   }
 }
 
-resource "null_resource" "upload_developer_index" {
+resource "null_resource" "upload_developer_external_api_v1" {
   triggers = {
-    dir_sha1 = sha1(join("", [for f in fileset(format("./env/%s/openapi", var.env), "**") : filesha1("./env/${var.env}/openapi/${f}")]))
+    dir_sha1 = sha1(join("", [for f in fileset(format("./env/%s/developer/external", var.env), "**") : filesha1("./env/${var.env}/developer/external/${f}")]))
   }
   
-  depends_on = [null_resource.read_apim_api]
+  depends_on = [null_resource.download_apim_external_api_v1]
 
   provisioner "local-exec" {
     command = <<EOT
@@ -37,13 +37,36 @@ resource "null_resource" "upload_developer_index" {
                 --container '$web' \
                 --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
                 --account-key ${module.checkout_cdn.storage_primary_access_key} \
-                --source "./env/${var.env}/openapi" \
-                --destination 'openapi/'
+                --source "./env/${var.env}/developer/external" \
+                --destination 'developer/external/'
               az cdn endpoint purge \
                 -g ${azurerm_resource_group.checkout_fe_rg.name} \
                 -n ${module.checkout_cdn.name} \
                 --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")}  \
-                --content-paths "/openapi/*" \
+                --content-paths "/developer/external/*" \
+                --no-wait
+          EOT
+  }
+}
+
+resource "null_resource" "upload_developer_index" {
+  triggers = {
+    dir_sha1 = sha1(filesha1("./env/${var.env}/developer/external/index.html"))
+  }
+  
+  provisioner "local-exec" {
+    command = <<EOT
+              az storage blob sync \
+                --container '$web' \
+                --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
+                --account-key ${module.checkout_cdn.storage_primary_access_key} \
+                --source "./env/${var.env}/developer/external/index.html" \
+                --destination 'developer/external/index.html'
+              az cdn endpoint purge \
+                -g ${azurerm_resource_group.checkout_fe_rg.name} \
+                -n ${module.checkout_cdn.name} \
+                --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")}  \
+                --content-paths "/developer/external/index.html" \
                 --no-wait
           EOT
   }
