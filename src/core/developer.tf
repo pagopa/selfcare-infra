801abd3a756a2007b2a2a0501a53465204100090
andrea-putzu <andrea.putzu@pagopa.it>
2022-09-21 13:29:53 +0200

    fixing

--- src/core/developer.tf
+++ src/core/developer.tf
@@ -6,11 +6,11 @@ locals {
 }
 
 resource "null_resource" "download_apim_external_api_v1" {
-  triggers = {
-    file_sha1 = filesha1("./api/ms_external_api/v1/open-api.yml.tpl")
-  }
+  #triggers = {
+  #  file_sha1 = filesha1("./api/ms_external_api/v1/open-api.yml.tpl")
+  #}
 
-  depends_on = [module.apim_external_api_ms_v1]
+  #depends_on = [module.apim_external_api_ms_v1]
 
   provisioner "local-exec" {
     command = <<EOT
@@ -26,19 +26,20 @@ resource "null_resource" "download_apim_external_api_v1" {
 
 resource "null_resource" "upload_developer_external_api_v1" {
   triggers = {
-    file_sha1 = filesha1("./api/ms_external_api/v1/open-api.yml.tpl")
+  #  file_sha1 = filesha1("./api/ms_external_api/v1/open-api.yml.tpl")
+    build_number = "${timestamp()}"
   }
 
   depends_on = [null_resource.download_apim_external_api_v1]
 
   provisioner "local-exec" {
     command = <<EOT
-              az storage blob sync \
+              az storage blob upload \
                 --container '$web' \
                 --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
                 --account-key ${module.checkout_cdn.storage_primary_access_key} \
-                --source "${path.module}/.terraform/tmp/env/${var.env}/developer/external/ms-external-api-v1.yaml" \
-                --destination 'developer/external/ms-external-api-v1.yaml'
+                --file "${path.module}/.terraform/tmp/env/${var.env}/developer/external/ms-external-api-v1.yaml" \
+                --name 'developer/external/ms-external-api-v1.yaml'
               az cdn endpoint purge \
                 --resource-group ${azurerm_resource_group.checkout_fe_rg.name} \
                 --name ${module.checkout_cdn.name} \
@@ -56,12 +57,12 @@ resource "null_resource" "upload_developer_index" {
 
   provisioner "local-exec" {
     command = <<EOT
-              az storage blob sync \
+              az storage blob upload \
                 --container '$web' \
                 --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
                 --account-key ${module.checkout_cdn.storage_primary_access_key} \
-                --source "./env/${var.env}/developer/external/index.html" \
-                --destination 'developer/external/index.html'
+                --file "./env/${var.env}/developer/external/index.html" \
+                --name 'developer/external/index.html'
               az cdn endpoint purge \
                 --resource-group ${azurerm_resource_group.checkout_fe_rg.name} \
                 --name ${module.checkout_cdn.name} \
@@ -71,4 +72,3 @@ resource "null_resource" "upload_developer_index" {
           EOT
   }
 }
-
