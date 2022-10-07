6efc5d442955f8bb54e269d5f756c31e71930eeb
USERSAD\bgalgamu <bgalgamu@emeal.nttdata.com>
2022-10-07 18:17:38 +0200

    fixed xml path

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -401,7 +401,7 @@ module "apim_external_api_ms_v1" {
     },
     {
       operation_id = "getInstitutionProductUsers"
-      xml_content=templatefile("./api/ms_external_api/getInstitutionProductUsers.xml.tpl",{
+      xml_content = templatefile("./api/ms_external_api/getInstitutionProductUsers_op_policy.xml.tpl", {
         MS_SELFCARE_DASHBOARD_BACKEND_BASE_URL = "http://${var.reverse_proxy_ip}/dashboard/v1/"
       })
     }
