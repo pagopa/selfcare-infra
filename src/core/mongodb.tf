cb8d401c136155277e501986e63b4e758b9be4ca
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2022-11-14 16:13:01 +0100

    feat: Cosmosdb ignore autoscale settings (#223)
    * ignore autoscale settings for cosmosdb
    
    * upgrade mongodb collection to ignore autoscale

--- src/core/mongodb.tf
+++ src/core/mongodb.tf
@@ -68,6 +68,12 @@ resource "azurerm_cosmosdb_mongo_database" "selc_product" {
       max_throughput = var.cosmosdb_mongodb_max_throughput
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      autoscale_settings
+    ]
+  }
 }
 
 resource "azurerm_management_lock" "mongodb_selc_product" {
@@ -90,6 +96,12 @@ resource "azurerm_cosmosdb_mongo_database" "selc_user_group" {
       max_throughput = var.cosmosdb_mongodb_max_throughput
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      autoscale_settings
+    ]
+  }
 }
 
 resource "azurerm_management_lock" "mongodb_selc_user_group" {
@@ -110,7 +122,7 @@ resource "azurerm_key_vault_secret" "cosmosdb_account_mongodb_connection_strings
 
 # Collections
 module "mongdb_collection_products" {
-  source = "git::https://github.com/pagopa/azurerm.git//cosmosdb_mongodb_collection?ref=v2.3.0"
+  source = "git::https://github.com/pagopa/azurerm.git//cosmosdb_mongodb_collection?ref=v3.3.0"
 
   name                = "products"
   resource_group_name = azurerm_resource_group.mongodb_rg.name
@@ -132,7 +144,7 @@ module "mongdb_collection_products" {
 }
 
 module "mongdb_collection_user-groups" {
-  source = "git::https://github.com/pagopa/azurerm.git//cosmosdb_mongodb_collection?ref=v2.3.0"
+  source = "git::https://github.com/pagopa/azurerm.git//cosmosdb_mongodb_collection?ref=v3.3.0"
 
   name                = "userGroups"
   resource_group_name = azurerm_resource_group.mongodb_rg.name
