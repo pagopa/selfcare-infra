55c860459595308c5f66d2684f838c0f55c1b5d9
bgalgamu <kevin.si1396@gmail.com>
2023-01-31 19:55:45 +0100

    fix external api service url version

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -365,7 +365,7 @@ module "apim_external_api_ms_v2" {
   protocols = [
   "https"]
 
-  service_url = format("http://%s/external-api/v2/", var.reverse_proxy_ip)
+  service_url = format("http://%s/external-api/v1/", var.reverse_proxy_ip)
 
   content_format = "openapi"
   content_value = templatefile("./api/ms_external_api/v2/open-api.yml.tpl", {
