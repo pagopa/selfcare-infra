72884dc43f8a762f3d3ab5a853e76070f730496c
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2023-02-03 17:15:42 +0100

    Update apim and update open-api for geoTaxonomies

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -461,7 +461,7 @@ module "apim_external_api_ms_v2" {
       operation_id = "getInstitutionGeographicTaxonomiesUsingGET"
       xml_content = templatefile("./api/ms_external_api/v2/getInstitutionGeographicTaxonomiesUsingGET_op_policy.xml.tpl", {
         API_DOMAIN                 = local.api_domain
-        ID                         = module.jwt.jwt_kid
+        KID                        = module.jwt.jwt_kid
         JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
       })
     },
