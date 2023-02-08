aa4e912ee586bc45c684abc883571d30e0e01062
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-08 22:36:24 +0100

    minor fix

--- src/k8s/02_serviceaccounts.tf
+++ src/k8s/02_serviceaccounts.tf
@@ -96,6 +96,10 @@ data "kubernetes_secret" "azure_devops_secret" {
   }
 }
 
+#
+# AKS service account infos -> KV
+#
+
 #tfsec:ignore:AZU023
 resource "azurerm_key_vault_secret" "azure_devops_sa_token" {
   depends_on   = [kubernetes_service_account.azure_devops]
@@ -111,6 +115,19 @@ resource "azurerm_key_vault_secret" "azure_devops_sa_cacrt" {
   depends_on   = [kubernetes_service_account.azure_devops]
   name         = "aks-azure-devops-sa-cacrt"
   value        = data.kubernetes_secret.azure_devops_secret.binary_data["ca.crt"] # base64 value
+  content_type = "text/plain"
+
+  key_vault_id = local.key_vault_id
+}
+
+locals {
+  aks_api_url = data.azurerm_kubernetes_cluster.aks.private_fqdn
+}
+
+#tfsec:ignore:AZU023
+resource "azurerm_key_vault_secret" "aks_apiserver_url" {
+  name         = "${var.aks_name}-apiserver-url"
+  value        = "https://${local.aks_api_url}:443"
   content_type = "text/plain"
 
   key_vault_id = local.key_vault_id
--- src/k8s/99_variables.tf
+++ src/k8s/99_variables.tf
@@ -1,3 +1,25 @@
+locals {
+  project                         = format("%s-%s", var.prefix, var.env_short)
+
+  key_vault_name                  = format("%s-kv", local.project)
+  key_vault_resource_group        = format("%s-sec-rg", local.project)
+  key_vault_id                    = "${data.azurerm_subscription.current.id}/resourceGroups/${local.key_vault_resource_group}/providers/Microsoft.KeyVault/vaults/${local.key_vault_name}"
+  
+  redis_url                       = "${format("%s-redis", local.project)}.redis.cache.windows.net"
+  
+  postgres_hostname               = "${format("%s-postgresql", local.project)}.postgres.database.azure.com"
+  postgres_replica_hostname       = var.enable_postgres_replica ? "${format("%s-postgresql-rep", local.project)}.postgres.database.azure.com" : local.postgres_hostname
+  
+  mongodb_name_selc_product       = "selcProduct"
+  mongodb_name_selc_user_group    = "selcUserGroup"
+  
+  contracts_storage_account_name  = replace(format("%s-contracts-storage", local.project), "-", "")
+  contracts_storage_container     = format("%s-contracts-blob", local.project)
+  
+  appinsights_instrumentation_key = format("InstrumentationKey=%s", module.key_vault_secrets_query.values["appinsights-instrumentation-key"].value)
+
+}
+
 variable "location" {
   type    = string
   default = "westeurope"
@@ -14,6 +36,16 @@ variable "env" {
 
 variable "env_short" {
   type = string
+}
+
+variable "aks_name" {
+  type = string
+  description = "AKS Name"
+}
+
+variable "aks_resource_group_name" {
+  type = string
+  description = "AKS resource group name"
 }
 
 variable "k8s_kube_config_path" {
