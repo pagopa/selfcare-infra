a16f74785fd11ae56b4524bcbb3650b7953fcd6b
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2023-02-10 10:52:46 +0100

    update apim and add policy for pn_cert, pn_hotfix and pn_prod

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -430,6 +430,9 @@ module "apim_external_api_ms_v2" {
     module.apim_product_pn_svil.product_id,
     module.apim_product_pn_dev.product_id,
     module.apim_product_pn_coll.product_id,
+    module.apim_product_pn_cert.product_id,
+    module.apim_product_pn_hotfix.product_id,
+    module.apim_product_pn_prod.product_id,
     module.apim_product_pagopa.product_id,
     module.apim_product_idpay.product_id
   ]
@@ -573,6 +576,57 @@ module "apim_product_pn_coll" {
   approval_required     = false
 
   policy_xml = file("./api_product/pn_coll/policy.xml")
+}
+
+module "apim_product_pn_hotfix" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.16"
+
+  product_id   = "pn-hotfix"
+  display_name = "PN HOTFIX"
+  description  = "Piattaforma Notifiche"
+
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/pn_hotfix/policy.xml")
+}
+
+module "apim_product_pn_cert" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.16"
+
+  product_id   = "pn-cert"
+  display_name = "PN CERT"
+  description  = "Piattaforma Notifiche"
+
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/pn_cert/policy.xml")
+}
+
+module "apim_product_pn_prod" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.16"
+
+  product_id   = "pn-prod"
+  display_name = "PN PROD"
+  description  = "Piattaforma Notifiche"
+
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  published             = true
+  subscription_required = true
+  approval_required     = false
+
+  policy_xml = file("./api_product/pn_prod/policy.xml")
 }
 
 module "apim_product_pagopa" {
