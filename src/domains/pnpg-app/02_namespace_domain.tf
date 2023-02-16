8a756c75b0d5af6c9a793c1bf880005146ea611a
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-16 16:35:05 +0100

    pre-commit fixs

--- src/domains/pnpg-app/02_namespace_domain.tf
+++ src/domains/pnpg-app/02_namespace_domain.tf
@@ -5,7 +5,7 @@ resource "kubernetes_namespace" "domain_namespace" {
 }
 
 module "domain_pod_identity" {
-source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_pod_identity?ref=v4.1.17"
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_pod_identity?ref=v4.1.17"
   resource_group_name = local.aks_resource_group_name
   location            = var.location
   tenant_id           = data.azurerm_subscription.current.tenant_id
@@ -13,7 +13,7 @@ source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_po
 
   identity_name = "${var.domain}-pod-identity"
   namespace     = kubernetes_namespace.domain_namespace.metadata[0].name
-  key_vault_id     = data.azurerm_key_vault.kv_domain.id
+  key_vault_id  = data.azurerm_key_vault.kv_domain.id
 
   secret_permissions = ["Get"]
 }
