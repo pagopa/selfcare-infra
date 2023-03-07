9b559c4a513bd1822d291de3b9409674e78c5221
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-03-07 16:25:32 +0100

    keyvault better file names

---
+++ src/core/01_key_vault_operations.tf
@@ -0,0 +1,112 @@
+data "azurerm_key_vault_certificate" "app_gw_platform" {
+  name         = var.app_gateway_api_certificate_name
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "monitor_notification_slack_email" {
+  name         = "monitor-notification-slack-email"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "monitor_notification_email" {
+  name         = "monitor-notification-email"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "apim_publisher_email" {
+  name         = "apim-publisher-email"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "sec_workspace_id" {
+  count        = var.env_short == "p" ? 1 : 0
+  name         = "sec-workspace-id"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "sec_storage_id" {
+  count        = var.env_short == "p" ? 1 : 0
+  name         = "sec-storage-id"
+  key_vault_id = module.key_vault.id
+}
+
+# JWT
+module "jwt" {
+  source = "git::https://github.com/pagopa/azurerm.git//jwt_keys?ref=v2.12.1"
+
+  jwt_name         = "jwt"
+  key_vault_id     = module.key_vault.id
+  cert_common_name = "apim"
+  cert_password    = ""
+  tags             = var.tags
+}
+
+module "jwt_exchange" {
+  source = "git::https://github.com/pagopa/azurerm.git//jwt_keys?ref=v2.12.1"
+
+  jwt_name         = "jwt-exchange"
+  key_vault_id     = module.key_vault.id
+  cert_common_name = "selfcare.pagopa.it"
+  cert_password    = ""
+  tags             = var.tags
+}
+
+module "agid_spid" {
+  source = "git::https://github.com/pagopa/azurerm.git//jwt_keys?ref=v3.8.1"
+
+  jwt_name         = "agid-spid"
+  key_vault_id     = module.key_vault.id
+  cert_common_name = "selfcare.pagopa.it"
+  cert_password    = ""
+  tags             = var.tags
+}
+
+resource "null_resource" "upload_jwks" {
+  triggers = {
+    "changes-in-jwt" : module.jwt.certificate_data_pem
+    "changes-in-jwt-exchange" : module.jwt_exchange.certificate_data_pem
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+              mkdir -p "${path.module}/.terraform/tmp"
+              pip install --require-hashes --requirement "${path.module}/utils/py/requirements.txt"
+              az storage blob download \
+                --container-name '$web' \
+                --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
+                --account-key ${module.checkout_cdn.storage_primary_access_key} \
+                --file "${path.module}/.terraform/tmp/oldJwks.json" \
+                --name '.well-known/jwks.json'
+              python "${path.module}/utils/py/jwksFromPems.py" "${path.module}/.terraform/tmp/oldJwks.json" "${module.jwt.jwt_kid}" "${module.jwt.certificate_data_pem}" "${module.jwt_exchange.jwt_kid}" "${module.jwt_exchange.certificate_data_pem}" > "${path.module}/.terraform/tmp/jwks.json"
+              if [ $? -eq 1 ]
+              then
+                exit 1
+              fi
+              az storage blob upload \
+                --container-name '$web' \
+                --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
+                --account-key ${module.checkout_cdn.storage_primary_access_key} \
+                --file "${path.module}/.terraform/tmp/jwks.json" \
+                --overwrite true \
+                --name '.well-known/jwks.json'
+              az cdn endpoint purge \
+                --resource-group ${azurerm_resource_group.checkout_fe_rg.name} \
+                --name ${module.checkout_cdn.name} \
+                --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")} \
+                --content-paths "/.well-known/jwks.json" \
+                --no-wait
+          EOT
+  }
+}
+
+resource "pkcs12_from_pem" "jwt_pkcs12" {
+  password        = ""
+  cert_pem        = module.jwt.certificate_data_pem
+  private_key_pem = module.jwt.jwt_private_key_pem
+}
+
+resource "azurerm_api_management_certificate" "jwt_certificate" {
+  name                = "jwt-spid-crt"
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+  data                = pkcs12_from_pem.jwt_pkcs12.result
+}
