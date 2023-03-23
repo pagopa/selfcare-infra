b32e3bdf3bec70a5f51c65bac28c3836928ee3cb
andrea-putzu <andrea.putzu@pagopa.it>
2023-03-23 18:20:53 +0100

    fix: Update inner services

--- src/domains/pnpg-app/10_selc_configmaps.tf
+++ src/domains/pnpg-app/10_selc_configmaps.tf
@@ -15,7 +15,7 @@ resource "kubernetes_config_map" "inner-service-url" {
     MS_USER_GROUP_URL                 = "http://ms-user-group:8080"
     MS_CORE_URL                       = "http://ms-core:8080"
     USERVICE_PARTY_PROCESS_URL        = "http://ms-core:8080"
-    USERVICE_PARTY_MANAGEMENT_URL     = "http://interop-be-party-management:8088/party-management/0.1"
+    USERVICE_PARTY_MANAGEMENT_URL     = "http://ms-core:8080"
     USERVICE_PARTY_REGISTRY_PROXY_URL = "http://ms-party-registry-proxy:8080/v1"
     MOCK_SERVER                       = "http://mock-server:1080"
   }
