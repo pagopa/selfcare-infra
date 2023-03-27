b3b357007bf1b5f8e7f1e4ed1b244fdc20bd95ee
bgalgamu <kevin.si1396@gmail.com>
2023-03-27 16:19:45 +0200

    add ms_external_api v2 for pnpg usage

--- src/domains/pnpg-app/06_selc_apim_pnpg.tf
+++ src/domains/pnpg-app/06_selc_apim_pnpg.tf
@@ -65,7 +65,7 @@ module "apim_external_api_data_vault_v1" {
   service_url = format("http://%s/external-api/v1/", var.reverse_proxy_ip)
 
   content_format = "openapi"
-  content_value  = templatefile("./api/external_api_data_vault/v1/open-api.yml.tpl", {
+  content_value  = templatefile("./api/external_api_data_vault/v1/open-api.yml.tpl", { 
     host     = local.pnpg_hostname
     basePath = "v1"
   })
@@ -88,7 +88,72 @@ module "apim_external_api_data_vault_v1" {
   ]
 }
 
+resource "azurerm_api_management_api_version_set" "apim_external_api_v2_for_pnpg"{
+  name                = format("%s-ms-external-api", var.env_short)
+  resource_group_name = local.apim_rg
+  api_management_name = local.apim_name
+  display_name        = "External API Service"
+  versioning_scheme   = "Segment"
+}
 
+module "apim_external_api_ms_v2" {
+  source              = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.12.5"
+  name                = format("%s-ms-external-api-pnpg", local.project)
+  api_management_name = local.apim_name
+  resource_group_name = local.apim_rg
+  version_set_id      = azurerm_api_management_api_version_set.apim_external_api_v2_for_pnpg.id
+
+  description  = "This service is the proxy for external services"
+  display_name = "External API service for PNPG"
+  path         = "external/pn-pg"
+  api_version  = "v2"
+  protocols    = [
+    "https"
+  ]
+
+  service_url = format("http://%s/external-api/v1/", var.reverse_proxy_ip)
+
+  content_format = "openapi"
+  content_value  = templatefile("./api/ms_external_api_for_pnpg/v2/open-api.yml.tpl", {
+    host     = local.pnpg_hostname
+    basePath = "v1"
+  })
+
+  xml_content = file("./api/ms_external_api_for_pnpg/v2/base_policy.xml")
+
+  subscription_required = true
+  product_ids           = [
+    module.apim_data_vault_product_pn_pg.product_id
+  ]
+
+  api_operation_policies = [
+    {
+      operation_id = "getInstitutionsUsingGET"
+      xml_content  = templatefile("./api/ms_external_api/v2/getInstitutions_op_policy.xml.tpl", {
+        CDN_STORAGE_URL                = "https://${local.cdn_storage_hostname}"
+      })
+    },
+    {
+      operation_id = "getUserGroupsUsingGET"
+      xml_content  = templatefile("./api/ms_external_api/v2/jwt_auth_op_policy_user_group.xml.tpl", {
+        USER_GROUP_BACKEND_BASE_URL = "http://${var.reverse_proxy_ip}/ms-user-group/user-groups/v1/"
+      })
+    },
+    {
+      operation_id = "getInstitution"
+      xml_content  = templatefile("./api/ms_external_api/v2/getInstitution_op_policy.xml.tpl", {
+        CDN_STORAGE_URL                = "https://${local.cdn_storage_hostname}"
+        PARTY_PROCESS_BACKEND_BASE_URL = "http://${var.reverse_proxy_ip}/ms-core/v1/"
+      })
+    },
+    {
+      operation_id = "getProductUsingGET"
+      xml_content  = templatefile("./api/ms_external_api/v2/getProduct_op_policy.xml.tpl", {
+        MS_PRODUCT_BACKEND_BASE_URL = "http://${var.reverse_proxy_ip}/ms-product/v1/"
+      })
+    }
+  ]
+}
 resource "azurerm_api_management_named_value" "apim_named_value_backend_access_token" {
 
   name                = "backend-access-token-pnpg"
