ab19c9f25f73c12d0eb64785415cfc902d5e71f2
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2023-05-08 12:26:49 +0200

    modified env for USERVICE_PARTY_MANAGEMENT_URL (#328)

--- src/k8s/selc_configmaps.tf
+++ src/k8s/selc_configmaps.tf
@@ -14,7 +14,7 @@ resource "kubernetes_config_map" "inner-service-url" {
     MS_NOTIFICATION_MANAGER_URL       = "http://ms-notification-manager:8080"
     MS_USER_GROUP_URL                 = "http://ms-user-group:8080"
     USERVICE_PARTY_PROCESS_URL        = var.env == "dev" ? "http://ms-core:8080" : format("http://interop-be-party-process:8088/party-process/%s", var.api-version_uservice-party-process)
-    USERVICE_PARTY_MANAGEMENT_URL     = format("http://interop-be-party-management:8088/party-management/%s", var.api-version_uservice-party-management)
+    USERVICE_PARTY_MANAGEMENT_URL     = var.env == "dev" ? "http://ms-core:8080" : format("http://interop-be-party-management:8088/party-management/%s", var.api-version_uservice-party-management)
     USERVICE_PARTY_REGISTRY_PROXY_URL = format("http://ms-party-registry-proxy:8080/%s", var.api-version_uservice-party-registry-proxy)
     MOCK_SERVER                       = "http://mock-server:1080"
   }
