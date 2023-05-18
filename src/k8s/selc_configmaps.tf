0b0e890e28570388a7e383f539855f896dceb682
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2023-05-18 15:23:57 +0200

    refactor base path

--- src/k8s/selc_configmaps.tf
+++ src/k8s/selc_configmaps.tf
@@ -15,7 +15,7 @@ resource "kubernetes_config_map" "inner-service-url" {
     MS_USER_GROUP_URL                 = "http://ms-user-group:8080"
     USERVICE_PARTY_PROCESS_URL        = var.env == "dev" ? "http://ms-core:8080" : format("http://interop-be-party-process:8088/party-process/%s", var.api-version_uservice-party-process)
     USERVICE_PARTY_MANAGEMENT_URL     = var.env == "dev" ? "http://ms-core:8080" : format("http://interop-be-party-management:8088/party-management/%s", var.api-version_uservice-party-management)
-    USERVICE_PARTY_REGISTRY_PROXY_URL = format("http://ms-party-registry-proxy:8080/%s", var.api-version_uservice-party-registry-proxy)
+    USERVICE_PARTY_REGISTRY_PROXY_URL = "http://ms-party-registry-proxy:8080"
     MOCK_SERVER                       = "http://mock-server:1080"
   }
 }
