36e8eef915d3bd61c1cf3cf20e3d89a1275bfd4a
andrea-putzu <andrea.putzu@pagopa.it>
2023-05-19 09:47:23 +0200

    fix: PNPG prod-pn-test

--- src/domains/pnpg-app/06_selc_apim_pnpg.tf
+++ src/domains/pnpg-app/06_selc_apim_pnpg.tf
@@ -77,7 +77,8 @@ module "apim_external_api_data_vault_v1" {
     module.apim_product_pnpg_uat_svil.product_id,
     module.apim_product_pnpg_uat.product_id,
     module.apim_product_pnpg_dev.product_id,
-    module.apim_product_pnpg_uat_cert.product_id
+    module.apim_product_pnpg_uat_cert.product_id,
+    module.apim_product_pnpg_test.product_id
   ]
 
   api_operation_policies = [
