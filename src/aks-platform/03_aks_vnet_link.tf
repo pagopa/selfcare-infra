a651fedda3ebe03ab0f64b55c7d948a1dec5d3b8
fmlisco <{ID}+{username}@users.noreply.github.com>
2023-06-01 16:06:20 +0200

    remove 2nd null_resource

--- src/aks-platform/03_aks_vnet_link.tf
+++ src/aks-platform/03_aks_vnet_link.tf
@@ -4,46 +4,6 @@
 
 # vnet needs a vnet link with aks private dns zone
 # aks terraform module doesn't export private dns zone
-resource "null_resource" "aks_dns_private_link_to_vnet_core" {
-
-  count = var.aks_enabled && var.aks_private_cluster_enabled ? 1 : 0
-  triggers = {
-    cluster_name = module.aks[0].name
-    vnet_id      = data.azurerm_virtual_network.vnet_core.id
-    vnet_name    = data.azurerm_virtual_network.vnet_core.name
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      dns_zone_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{name:name}")
-      dns_zone_resource_group_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{resourceGroup:resourceGroup}")
-      az network private-dns link vnet create \
-        --name ${self.triggers.vnet_name} \
-        --registration-enabled false \
-        --resource-group $dns_zone_resource_group_name \
-        --virtual-network ${self.triggers.vnet_id} \
-        --zone-name $dns_zone_name
-    EOT
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      dns_zone_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{name:name}")
-      dns_zone_resource_group_name=$(az network private-dns zone list --output tsv --query "[?contains(id,'${self.triggers.cluster_name}')].{resourceGroup:resourceGroup}")
-      az network private-dns link vnet delete \
-        --name ${self.triggers.vnet_name} \
-        --resource-group $dns_zone_resource_group_name \
-        --zone-name $dns_zone_name \
-        --yes
-    EOT
-  }
-
-  depends_on = [
-    module.aks
-  ]
-}
-
 # linking vnet AKS to vnet pair
 data "external" "get_dns_zone" {
   program = ["bash", "-c", <<-EOH
@@ -53,7 +13,16 @@ data "external" "get_dns_zone" {
   EOH
   ]
 }
+resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_to_vnet_core" {
+  name                  = data.azurerm_virtual_network.vnet_core.name
+  resource_group_name   = data.external.get_dns_zone.result["dns_zone_resource_group_name"]
+  private_dns_zone_name = data.external.get_dns_zone.result["dns_zone_name"]
+  virtual_network_id    = data.azurerm_virtual_network.vnet_core.id
 
+  depends_on = [module.aks]
+}
+
+# linking vnet AKS to vnet pair
 resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_private_link_to_vnet_pair" {
   name                  = data.azurerm_virtual_network.vnet_pair.name
   resource_group_name   = data.external.get_dns_zone.result["dns_zone_resource_group_name"]
