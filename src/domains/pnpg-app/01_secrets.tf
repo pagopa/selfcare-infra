e157592a7df46f324a322f9792943c7735f8d1a0
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2023-06-07 11:15:12 +0200

    fix: Modified support mail (#356)
    * fix : modified support mail
    
    * #tfsec:ignore:azure-keyvault-ensure-secret-expiry
    
    * #tfsec:ignore:azure-keyvault-ensure-secret-expiry

--- src/domains/pnpg-app/01_secrets.tf
+++ src/domains/pnpg-app/01_secrets.tf
@@ -1,3 +1,4 @@
+#tfsec:ignore:azure-keyvault-ensure-secret-expiry
 resource "azurerm_key_vault_secret" "appinsights-instrumentation-key" {
   key_vault_id = data.azurerm_key_vault.kv_domain.id
   name         = "appinsights-instrumentation-key"
