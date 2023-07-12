f7a1576b7cb6d3368350a3143822202081e81e4c
manuraf <manuel.rafeli@pagopa.it>
2023-07-12 18:05:00 +0200

    feat: added pnpg_error_5xx_uat metric

--- src/domains/pnpg-app/99_variables.tf
+++ src/domains/pnpg-app/99_variables.tf
@@ -50,6 +50,11 @@ locals {
   cdn_rg_name     = "${local.project}-checkout-fe-rg"
   cdn_fqdn_url    = "https://${module.key_vault_secrets_query.values["cdn-fqdn"].value}"
   cdn_storage_url = "https://${module.key_vault_secrets_query.values["cdn-storage-blob-primary-web-host"].value}"
+
+
+  # Monitor
+  action_group_pnpg_uat_name = "PnpgUAT"
+  alert_pnpg_error_5xx_name = "pnpg-error-5xx"
 }
 
 variable "prefix" {
