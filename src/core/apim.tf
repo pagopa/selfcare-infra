30edbacaf398b2f837c5912e37ffb88d0302a045
pierpaolodidato89 <pierpaolo.didato@emeal.nttdata.com>
2023-07-19 09:04:43 +0200

    Created policy for get user info api

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -590,7 +590,7 @@ module "apim_external_api_ms_v2" {
     },
     {
       operation_id = "getUserInfoUsingPOST"
-      xml_content = templatefile("./api/ms_external_api/v2/jwt_auth_op_policy_user_group.xml.tpl", {
+      xml_content = templatefile("./api/ms_external_api/v2/getUserInfo_op_policy.xml.tpl", {
         API_DOMAIN                 = local.api_domain
         KID                        = module.jwt.jwt_kid
         JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
