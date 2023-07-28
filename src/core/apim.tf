151f6b6a3d271f5aaff21a08ffc89f548437a71c
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2023-07-28 10:50:06 +0200

    feat: added api to retrieve delegation

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -637,6 +637,14 @@ module "apim_external_api_ms_v2" {
         TENANT_ID                  = data.azurerm_client_config.current.tenant_id
         EXTERNAL-OAUTH2-ISSUER     = data.azurerm_key_vault_secret.external-oauth2-issuer.value
       })
+    },
+    {
+      operation_id = "getDelegationsUsingGET"
+      xml_content = templatefile("./api/ms_external_api/v2/getDelegationsUsingGET_op_policy.xml.tpl", {
+        API_DOMAIN                 = local.api_domain
+        KID                        = module.jwt.jwt_kid
+        JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
+      })
     }
   ]
 }
