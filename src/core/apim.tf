0a1a4dedf6c2a561b74f5fdd373c3f2b8041e390
pierpaolodidato89 <pierpaolo.didato@emeal.nttdata.com>
2023-08-03 16:57:40 +0200

    Fix set api for autoapproval onboarding

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -686,16 +686,6 @@ module "apim_external_api_ms_v2" {
         TENANT_ID                  = data.azurerm_client_config.current.tenant_id
         EXTERNAL-OAUTH2-ISSUER     = data.azurerm_key_vault_secret.external-oauth2-issuer.value
       })
-    },
-    {
-      operation_id = "onboardingUsingPOST"
-      xml_content = templatefile("./api/ms_external_api/v2/onboardingUsingPOST_op_policy.xml.tpl", {
-        API_DOMAIN                 = local.api_domain
-        KID                        = module.jwt.jwt_kid
-        JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
-        TENANT_ID                  = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER     = data.azurerm_key_vault_secret.external-oauth2-issuer.value
-      })
     }
   ]
 }
@@ -753,6 +743,10 @@ module "apim_internal_api_ms_v1" {
     },
     {
       operation_id = "autoApprovalOnboardingUsingPOST"
+      xml_content  = file("./api/jwt_auth_op_policy.xml")
+    },
+    {
+      operation_id = "onboardingUsingPOST"
       xml_content  = file("./api/jwt_auth_op_policy.xml")
     },
     {
