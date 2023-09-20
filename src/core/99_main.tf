de3e28a5bd1d92c691e36bbd14ce32098d074364
andrea-putzu <andrea.putzu@pagopa.it>
2023-09-20 16:43:23 +0200

    update deploy pipeline

--- src/core/99_main.tf
+++ src/core/99_main.tf
@@ -36,3 +36,13 @@ provider "azurerm" {
 data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
+
+provider "kubernetes" {
+  config_path = "${var.k8s_kube_config_path_prefix}/config-${local.aks_cluster_name}"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "${var.k8s_kube_config_path_prefix}/config-${local.aks_cluster_name}"
+  }
+}
