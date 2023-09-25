c2e64c8ec29bb0d2e9985ee83735991912a24152
Manuel Rafeli <manuel.rafeli@pagopa.it>
2023-09-25 11:38:04 +0200

    Added container apps and function environment (#454)

--- src/core/99_variables.tf
+++ src/core/99_variables.tf
@@ -1145,6 +1145,59 @@ variable "azdo_agent_vm_sku" {
   default     = "Standard_B1s"
 }
 
+variable "cidr_subnet_selc" {
+  type        = list(string)
+  description = "Address prefixes subnet selc ca and functions"
+  default     = null
+}
+
+# Storage account
+variable "storage_account_info" {
+  type = object({
+    account_kind                      = string
+    account_tier                      = string
+    account_replication_type          = string
+    access_tier                       = string
+    advanced_threat_protection_enable = bool
+  })
+
+  default = {
+    account_kind                      = "StorageV2"
+    account_tier                      = "Standard"
+    account_replication_type          = "LRS"
+    access_tier                       = "Hot"
+    advanced_threat_protection_enable = true
+  }
+}
+
+# App service plan
+variable "app_service_plan_info" {
+  type = object({
+    kind                         = string # The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux, elastic (for Premium Consumption) and FunctionApp (for a Consumption Plan).
+    sku_size                     = string # Specifies the plan's instance size.
+    sku_tier                     = string
+    maximum_elastic_worker_count = number # The maximum number of total workers allowed for this ElasticScaleEnabled App Service Plan.
+    worker_count                 = number # The number of Workers (instances) to be allocated.
+    zone_balancing_enabled       = bool   # Should the Service Plan balance across Availability Zones in the region. Changing this forces a new resource to be created.
+  })
+
+  description = "Allows to configurate the internal service plan"
+
+  default = {
+    kind                         = "Linux"
+    sku_size                     = "S1"
+    sku_tier                     = "StandardS1"
+    maximum_elastic_worker_count = 0
+    worker_count                 = 0
+    zone_balancing_enabled       = false
+  }
+}
+
+variable "function_always_on" {
+  type        = bool
+  description = "Always on property"
+  default     = false
+}
 
 variable "checkout_advanced_threat_protection_enabled" {
   type        = string
--- src/core/env/prod/terraform.tfvars
+++ src/core/env/prod/terraform.tfvars
@@ -35,6 +35,8 @@ cidr_subnet_private_endpoints     = ["10.1.140.0/24"]
 cidr_subnet_pnpg_cosmosdb_mongodb = ["10.1.141.0/24"] #this is a place holder for pnpg mongo
 cidr_subnet_load_tests            = ["10.1.142.0/29"]
 
+cidr_subnet_selc                  = ["10.1.148.0/23"]
+
 #
 # Pair VNET
 #
@@ -417,3 +419,23 @@ logs_advanced_threat_protection = true
 enable_load_tests_db = false
 
 checkout_advanced_threat_protection_enabled = true
+# Functions App
+
+function_always_on = true
+
+app_service_plan_info = {
+  kind                         = "Linux"
+  sku_size                     = "P1v3"
+  sku_tier                     = "PremiumV3"
+  maximum_elastic_worker_count = 1
+  worker_count                 = 1
+  zone_balancing_enabled       = false
+}
+
+storage_account_info = {
+  account_kind                      = "StorageV2"
+  account_tier                      = "Standard"
+  account_replication_type          = "LRS"
+  access_tier                       = "Hot"
+  advanced_threat_protection_enable = true
+}
