405e06a3f5afae5c862ad574dcb2cbbb9caef934
Andrea Grillo <andrea.grillo@pagopa.it>
2023-10-18 18:03:41 +0200

    Switch to GitHub runner Terraform module (#476)

--- src/core/99_main.tf
+++ src/core/99_main.tf
@@ -10,11 +10,6 @@ terraform {
       version = ">= 2.33.0"
     }
 
-    azapi = {
-      source  = "azure/azapi"
-      version = "<= 1.9.0"
-    }
-
     pkcs12 = {
       source  = "chilicat/pkcs12"
       version = "0.0.7"
@@ -36,8 +31,6 @@ provider "azurerm" {
     }
   }
 }
-
-provider "azapi" {}
 
 data "azurerm_subscription" "current" {}
 
