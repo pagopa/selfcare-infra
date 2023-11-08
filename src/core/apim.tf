aa4d9a72b6769ff0ed38b97629c873b3c2688072
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2023-11-08 09:14:08 +0100

    Fix path of new api for v2

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -704,7 +704,7 @@ module "apim_external_api_ms_v2" {
     },
     {
       operation_id = "sendSupportRequestUsingPOST"
-      xml_content = templatefile("./api/selfcare_support_service/v2/sendSupportRequest_op_policy.xml.tpl", {
+      xml_content = templatefile("./api/ms_external_api/v2/sendSupportRequest_op_policy.xml.tpl", {
         API_DOMAIN                 = local.api_domain
         KID                        = module.jwt.jwt_kid
         JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
