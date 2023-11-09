988777269c6b8f7998892e7e5353746317ff2492
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-11-09 14:57:08 +0100

    added ingress to selc

--- src/k8s/health_ingress.tf
+++ src/k8s/health_ingress.tf
@@ -14,6 +14,7 @@ resource "kubernetes_ingress_v1" "health_ingress" {
 
   spec {
     rule {
+      host = "selc.internal.dev.selfcare.pagopa.it"
       http {
         path {
           backend {
@@ -27,6 +28,11 @@ resource "kubernetes_ingress_v1" "health_ingress" {
           path = "/health(.*)"
         }
       }
+    }
+
+    tls {
+      hosts = ["selc.internal.dev.selfcare.pagopa.it"]
+      secret_name = "selc-internal-dev-selfcare-pagopa-it"
     }
   }
 }
