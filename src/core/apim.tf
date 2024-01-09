2490fcaf6e17dd6d2574be40e28d90991a15bfeb
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2024-01-09 16:22:46 +0100

    [SELC - 3687] feat: added complete onboarding without signature verification API to… (#590)

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -802,7 +802,15 @@ module "apim_internal_api_ms_v1" {
       operation_id = "onboardingInstitutionUsersUsingPOST"
       xml_content = templatefile("./api/ms_internal_api/v1/postOnboardingInstitutionUsers_op_policy.xml.tpl", {
         MS_CORE_BACKEND_BASE_URL = "http://${var.private_dns_name}/ms-core/v1/"
-      })
+        }
+      )
+    },
+    {
+      operation_id = "completeOnboardingTokenConsume"
+      xml_content = templatefile("./api/ms_internal_api/v1/core_op_policy.xml.tpl", {
+        MS_CORE_BACKEND_BASE_URL = "http://${var.private_dns_name}/ms-core/v1/"
+        }
+      )
     }
   ]
 }
