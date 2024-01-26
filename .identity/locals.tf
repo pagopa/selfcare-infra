1920ee66e220847928e82c1b52e02a38038259b1
Andrea Grillo <andrea.grillo@pagopa.it>
2024-01-26 18:03:14 +0100

    move sub to env secret

--- .identity/locals.tf
+++ .identity/locals.tf
@@ -10,15 +10,16 @@ locals {
   }
 
   repo_secrets = {
-    "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
-    "AZURE_TENANT_ID"       = data.azurerm_client_config.current.tenant_id,
+    "AZURE_TENANT_ID" = data.azurerm_client_config.current.tenant_id,
   }
 
   env_ci_secrets = {
-    "AZURE_CLIENT_ID_CI" = module.identity_ci.identity_client_id
+    "AZURE_CLIENT_ID_CI"    = module.identity_ci.identity_client_id
+    "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
   }
 
   env_cd_secrets = {
-    "AZURE_CLIENT_ID_CD" = module.identity_cd.identity_client_id
+    "AZURE_CLIENT_ID_CD"    = module.identity_cd.identity_client_id
+    "AZURE_SUBSCRIPTION_ID" = data.azurerm_client_config.current.subscription_id
   }
 }
