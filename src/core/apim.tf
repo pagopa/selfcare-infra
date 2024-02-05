7e4a4d8feee29acd0d9a9f5878de677484e60da4
Giulia Tremolada <giulia.tremolada@nttdata.com>
2024-02-05 15:58:42 +0100

    fix: change api variables for internal v1

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -780,6 +780,7 @@ module "apim_internal_api_ms_v1" {
       xml_content = templatefile("./api/ms_internal_api/v1/external_op_policy.xml.tpl", {
         API_DOMAIN                   = local.api_domain
         KID                          = module.jwt.jwt_kid
+        JWT_CERTIFICATE_THUMBPRINT   = azurerm_api_management_certificate.jwt_certificate.thumbprint
         MS_EXTERNAL_BACKEND_BASE_URL = "http://${var.private_dns_name}/external-api/v2/"
       })
     },
