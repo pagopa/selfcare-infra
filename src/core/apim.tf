2df5d151abd671e7cc48f2893d44ae3c68c02deb
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-02-09 08:57:33 +0100

    Changed reference of getContract to v2 into external api

--- src/core/apim.tf
+++ src/core/apim.tf
@@ -642,6 +642,7 @@ module "apim_external_api_ms_v2" {
     {
       operation_id = "getContractUsingGET"
       xml_content = templatefile("./api/ms_external_api/v2/getContractUsingGet_op_policy.xml.tpl", {
+        BACKEND_BASE_URL           = "http://${var.private_dns_name}/external-api/v2/")
         API_DOMAIN                 = local.api_domain
         KID                        = module.jwt.jwt_kid
         JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
