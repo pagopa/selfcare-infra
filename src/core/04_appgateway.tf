5c4007cc69888cca8ecaf0db86a33eff50b6670f
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-04-10 18:08:10 +0200

    Redirect hub-spid-login to Container App Environment (#660)
    * redirect selc
    
    * cae-cp url dev pnpg
    
    * ca_suffix_dns_private_name
    
    * redirect pnpg
    
    * fix rewrite path
    
    * spid_path_prefix var
    
    * fix fqdns for uat
    
    * prod cae url
    
    * pre commit

--- src/core/04_appgateway.tf
+++ src/core/04_appgateway.tf
@@ -66,6 +66,28 @@ locals {
       fqdns                       = ["${var.aks_platform_env}.pnpg.internal.${var.dns_zone_prefix}.${var.external_domain}"]
       pick_host_name_from_backend = false
     }
+    hub-spid-selc = {
+      protocol                    = "Https"
+      host                        = "selc-${var.env_short}-hub-spid-login-ca.${var.ca_suffix_dns_private_name}"
+      port                        = 443
+      ip_addresses                = null
+      probe                       = "/info"
+      probe_name                  = "probe-hub-spid-selc"
+      request_timeout             = 60
+      fqdns                       = ["selc-${var.env_short}-hub-spid-login-ca.${var.ca_suffix_dns_private_name}"]
+      pick_host_name_from_backend = false
+    }
+    hub-spid-pnpg = {
+      protocol                    = "Https"
+      host                        = "selc-${var.env_short}-pnpg-hub-spid-login-ca.${var.ca_pnpg_suffix_dns_private_name}"
+      port                        = 443
+      ip_addresses                = null
+      probe                       = "/info"
+      probe_name                  = "probe-hub-spid-pnpg"
+      request_timeout             = 60
+      fqdns                       = ["selc-${var.env_short}-pnpg-hub-spid-login-ca.${var.ca_pnpg_suffix_dns_private_name}"]
+      pick_host_name_from_backend = false
+    }
   }
 
   listeners = {
@@ -161,6 +183,11 @@ module "app_gw" {
           backend               = "apim"
           rewrite_rule_set_name = null
         }
+        hub_spid_selc = {
+          paths                 = ["${var.spid_path_prefix}/*"]
+          backend               = "hub-spid-selc"
+          rewrite_rule_set_name = "rewrite-rule-set-hub-spid"
+        }
       }
     }
     api-pnpg = {
@@ -171,6 +198,11 @@ module "app_gw" {
           paths                 = ["/imprese/dashboard/*", "/imprese/onboarding/*"]
           backend               = "apim"
           rewrite_rule_set_name = null
+        }
+        hub_spid_pnpg = {
+          paths                 = ["${var.spid_path_prefix}/*"]
+          backend               = "hub-spid-pnpg"
+          rewrite_rule_set_name = "rewrite-rule-set-hub-spid"
         }
       }
     }
@@ -233,6 +265,28 @@ module "app_gw" {
         },
       ]
     },
+    {
+      name = "rewrite-rule-set-hub-spid"
+      rewrite_rules = [
+        {
+          name          = "remove-spid-path"
+          rule_sequence = 1
+          conditions = [{
+            ignore_case = true
+            negate      = false
+            pattern     = "${var.spid_path_prefix}/(.*)"
+            variable    = "var_uri_path"
+          }]
+          request_header_configurations  = []
+          response_header_configurations = []
+          url = {
+            path         = "{var_uri_path_1}"
+            query_string = ""
+            components   = "path_only"
+          }
+        }
+      ]
+    }
   ]
 
   # TLS
