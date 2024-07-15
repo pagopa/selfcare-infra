7ac63e8c840494ae171004a3e2f1242a148a5eab
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-07-15 09:47:25 +0200

    Migrate Container App Environment DEV to New Workload Profile Environment (#742)
    * feat: change suffix selc cae
    
    * feat: change suffix domain app gw
    
    * feat: add association ip to nat gw
    
    * pre-commit

--- src/container-apps/prod/westeurope/main.tf
+++ src/container-apps/prod/westeurope/main.tf
@@ -48,9 +48,10 @@ module "networking" {
 module "container_app_environments" {
   source = "../../_modules/container_app_environments"
 
-  project             = local.project
-  location            = local.location
-  resource_group_name = azurerm_resource_group.selc_container_app_rg.name
+  project                  = local.project
+  location                 = local.location
+  selc_resource_group_name = azurerm_resource_group.selc_container_app_rg.name
+  pnpg_resource_group_name = azurerm_resource_group.selc_container_app_rg.name
 
   selc_subnet_id = module.networking.subnet_selfcare.id
   pnpg_subnet_id = module.networking.subnet_pnpg.id
