02560e7027a2246dbae01e687f54a3c4d41d6104
andrea-putzu <andrea.putzu@pagopa.it>
2024-07-25 17:26:09 +0200

    chore: Adding deploy pipeline for contracts

--- src/core/06_github.tf
+++ src/core/06_github.tf
@@ -4,11 +4,11 @@ data "github_repository" "repo" {
 
 resource "github_repository_environment" "github_repository_environment_cd" {
   environment = "${var.env}-cd"
-  repository  = data.github_repository.repo.full_name
+  repository  = data.github_repository.repo.name
 }
 
 resource "github_actions_environment_secret" "github_cd_secrets_storage" {
-  repository       = data.github_repository.repo.full_name
+  repository       = data.github_repository.repo.name
   environment      = github_repository_environment.github_repository_environment_cd.environment
   secret_name      = "STORAGE_CONTRACTS_ACCOUNT_KEY"
   plaintext_value  = azurerm_key_vault_secret.selc_contracts_storage_access_key.value
