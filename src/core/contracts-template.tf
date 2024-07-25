6c8eefe96336c9fff2bc267812b832c1107637be
andrea-putzu <andrea.putzu@pagopa.it>
2024-07-25 15:45:37 +0200

    Feat: Refactor for copy template contracts to storage

--- src/core/contracts-template.tf
+++ src/core/contracts-template.tf
@@ -1,14 +1,15 @@
 resource "null_resource" "upload_contract_templates" {
   triggers = {
-    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/contracts_template", "**") : filesha1("${path.module}/contracts_template/${f}")]))
+    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/contracts/template", "**") : filesha1("${path.module}/contracts/template/${f}")]))
   }
   provisioner "local-exec" {
     command = <<EOT
-              az storage blob sync --container ${azurerm_storage_container.selc-contracts-container.name} \
-                  --account-name ${module.selc-contracts-storage.name} \
-                  --account-key "${module.selc-contracts-storage.primary_access_key}" \
-                  --source "${path.module}/contracts_template/" \
-                  --destination contracts/template/
+              az storage copy \
+                --account-name ${module.selc-contracts-storage.name} \
+                --account-key "${module.selc-contracts-storage.primary_access_key}" \
+                --destination-container ${azurerm_storage_container.selc-contracts-container.name} \
+                --source "${path.module}/contracts/template/" \
+                --recursive
           EOT
   }
 }
