61687996483ffcfc157742e902d98a1076dc19e0
andrea-putzu <andrea.putzu@pagopa.it>
2024-07-25 13:13:31 +0200

    Adding new container for contract template

--- src/core/contracts-template.tf
+++ src/core/contracts-template.tf
@@ -4,12 +4,11 @@ resource "null_resource" "upload_contract_templates" {
   }
   provisioner "local-exec" {
     command = <<EOT
-              az storage blob sync --container ${azurerm_storage_container.selc-contracts-container.name} \
+              az storage blob sync --container ${azurerm_storage_container.selc-contracts-template-container.name} \
                   --account-name ${module.selc-contracts-storage.name} \
-                  --account-key "${module.selc-contracts-storyesage.primary_access_key}" \
+                  --account-key "${module.selc-contracts-storage.primary_access_key}" \
                   --source "${path.module}/contracts_template/" \
                   --destination contracts/template/
-                  --delete-destination false
           EOT
   }
 }
