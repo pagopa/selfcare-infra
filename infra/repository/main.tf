2735ca6a16ba96b628646af8b08bfb903c433e0b
manuraf <manuel.rafeli@pagopa.it>
2024-10-15 16:35:40 +0200

    fix: revert change static env

--- infra/repository/main.tf
+++ infra/repository/main.tf
@@ -1,14 +1,5 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "<= 3.111.0"
-    }
-    github = {
-      source  = "integrations/github"
-      version = "5.45.0"
-    }
-  }
+  required_version = ">= 1.6.0"
 
   backend "azurerm" {}
 }
