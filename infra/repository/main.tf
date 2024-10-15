813600ad9d8211d53516659a355be68ef66021d4
manuraf <manuel.rafeli@pagopa.it>
2024-10-15 16:32:21 +0200

    fix: set azurerm 3.111.0

--- infra/repository/main.tf
+++ infra/repository/main.tf
@@ -1,5 +1,14 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "<= 3.111.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.45.0"
+    }
+  }
 
   backend "azurerm" {}
 }
