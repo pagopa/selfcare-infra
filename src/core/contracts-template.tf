2a200848d00483c4b4316c7b51c2c2ad240e7da1
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2024-10-28 16:53:25 +0100

    Update deploy contracts with github action (#798)
    * refactor folders and resources for release contracts template
    
    * Update github action

--- src/core/contracts-template.tf
+++ src/core/contracts-template.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "upload_contract_templates" {
   triggers = {
-    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/contracts/template", "**") : filesha1("${path.module}/contracts/template/${f}")]))
+    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/contracts_template/contracts/template", "**") : filesha1("${path.module}/contracts_template/contracts/template/${f}")]))
   }
   provisioner "local-exec" {
     command = <<EOT
@@ -8,7 +8,7 @@ resource "null_resource" "upload_contract_templates" {
                 --account-name ${module.selc-contracts-storage.name} \
                 --account-key "${module.selc-contracts-storage.primary_access_key}" \
                 --destination-container ${azurerm_storage_container.selc-contracts-container.name} \
-                --source "${path.module}/src/core/contracts/*" \
+                --source "./contracts_template/*" \
                 --recursive
           EOT
   }
