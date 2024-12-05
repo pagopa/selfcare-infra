ce4ebcda0c47be4583710c9f79d5f056c1b42e92
andrea putzu <andrea.putzu@pagopa.it>
2024-12-05 16:41:27 +0100

    chrore: add gh action for publish web assets

--- src/core/contracts-template.tf
+++ src/core/contracts-template.tf
@@ -13,3 +13,56 @@ resource "null_resource" "upload_contract_templates" {
           EOT
   }
 }
+
+resource "null_resource" "upload_psp_contracts" {
+  provisioner "local-exec" {
+    command = <<EOT
+              az storage copy \
+                --account-name ${module.selc-contracts-storage.name} \
+                --account-key "${module.selc-contracts-storage.primary_access_key}" \
+                --destination-container ${nonsensitive(azurerm_storage_container.selc-contracts-container.name)} \
+                --source "./contracts_psp/*" \
+                --recursive
+          EOT
+  }
+}
+
+
+resource "null_resource" "upload_io_contracts" {
+  provisioner "local-exec" {
+    command = <<EOT
+              az storage copy \
+                --account-name ${module.selc-contracts-storage.name} \
+                --account-key "${module.selc-contracts-storage.primary_access_key}" \
+                --destination-container ${azurerm_storage_container.selc-contracts-container.name} \
+                --source "./contracts_io/*" \
+                --recursive
+          EOT
+  }
+}
+
+resource "null_resource" "app_io_premium_plans" {
+  # triggers = {
+  #   file_sha1 = filesha1("./env/${var.env}/assets/app-io-premium-plans.json")
+  # }
+
+  provisioner "local-exec" {
+    command = <<EOT
+              az storage blob upload \
+                --container '$web' \
+                --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
+                --account-key ${module.checkout_cdn.storage_primary_access_key} \
+                --file "./env/${var.env}/assets/app-io-premium-plans.json" \
+                --overwrite true \
+                --name 'assets/app-io-premium-plans.json'
+
+              az cdn endpoint purge \
+                --resource-group ${azurerm_resource_group.checkout_fe_rg.name} \
+                --name ${module.checkout_cdn.name} \
+                --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")}  \
+                --content-paths "/assets/app-io-premium-plans.json" \
+                --no-wait
+          EOT
+  }
+}
+
