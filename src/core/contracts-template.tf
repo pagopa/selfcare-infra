9093a75a272be378a432408894b2b028234c8951
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2025-02-17 11:47:14 +0100

    Fix deploy assets CDN (#843)
    * updating workflow
    
    * updating workflow
    
    * updating gh assets workflow
    
    * updating gh assets workflow
    
    * updating gh assets workflow

--- src/core/contracts-template.tf
+++ src/core/contracts-template.tf
@@ -14,27 +14,3 @@ resource "null_resource" "upload_contract_templates" {
   }
 }
 
-resource "null_resource" "app_io_premium_plans" {
-  # triggers = {
-  #   file_sha1 = filesha1("./env/${var.env}/assets/app-io-premium-plans.json")
-  # }
-
-  provisioner "local-exec" {
-    command = <<EOT
-              az storage blob upload \
-                --container '$web' \
-                --account-name ${replace(replace(module.checkout_cdn.name, "-cdn-endpoint", "-sa"), "-", "")} \
-                --account-key ${module.checkout_cdn.storage_primary_access_key} \
-                --file "./env/${var.env}/assets/app-io-premium-plans.json" \
-                --overwrite true \
-                --name 'assets/app-io-premium-plans.json'
-
-              az cdn endpoint purge \
-                --resource-group ${azurerm_resource_group.checkout_fe_rg.name} \
-                --name ${module.checkout_cdn.name} \
-                --profile-name ${replace(module.checkout_cdn.name, "-cdn-endpoint", "-cdn-profile")}  \
-                --content-paths "/assets/app-io-premium-plans.json" \
-                --no-wait
-          EOT
-  }
-}
