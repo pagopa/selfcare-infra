e57c568cf6d34c9995cb250a8f61a6c1f511be06
andrea putzu <andrea.putzu@pagopa.it>
2025-07-31 12:10:32 +0200

    chore: Update scripts

--- src/core/contracts-template.tf
+++ src/core/contracts-template.tf
@@ -1,21 +1,15 @@
 resource "null_resource" "upload_contract_templates" {
-  count        = local.enable_upload_contracts ? 1 : 0
   triggers = {
     dir_sha1 = sha1(join("", [for f in fileset("${path.module}/contracts_template/contracts/template", "**") : filesha1("${path.module}/contracts_template/contracts/template/${f}")]))
   }
   provisioner "local-exec" {
     command = <<EOT
               az storage copy \
-                --account-name ${module.selc-contracts-storage.name} \
-                --account-key "${module.selc-contracts-storage.primary_access_key}" \
-                --destination-container ${azurerm_storage_container.selc-contracts-container.name} \
+                --account-name "sc${var.env_short}${var.location_short}ardocumentsst01" \
+                --connection-string "${data.azurerm_key_vault_secret.selc_documents_storage_connection_string.value}" \
+                --destination-container "sc-${var.env_short}-documents-blob" \
                 --source "./contracts_template/*" \
                 --recursive
           EOT
   }
-}
-
-data "azurerm_key_vault_secret" "selc_documents_storage_connection_string" {
-  name         = "documents-storage-connection-string"
-  key_vault_id = module.key_vault.id
 }
