f7c083e8ebb3ffb0e4b38e826e550a6d76cdd692
andrea putzu <andrea.putzu@pagopa.it>
2025-08-01 12:45:40 +0200

    chore: Updating container app environment, adding user assign identity

--- src/container-apps/_modules/container_app_environments/outputs.tf
+++ src/container-apps/_modules/container_app_environments/outputs.tf
@@ -5,3 +5,33 @@ output "container_app_environment_selfcare" {
     resource_group_name = azurerm_container_app_environment.cae_selc.resource_group_name
   }
 }
+
+output "container_app_environment_pnpg" {
+  value = {
+    id                  = azurerm_container_app_environment.cae_pnpg.id
+    name                = azurerm_container_app_environment.cae_pnpg.name
+    resource_group_name = azurerm_container_app_environment.cae_pnpg.resource_group_name
+  }
+}
+
+output "user_assigned_identity_selfcare" {
+  value = {
+    id           = azurerm_user_assigned_identity.cae_identity_selc.id
+    name         = azurerm_user_assigned_identity.cae_identity_selc.name
+    client_id    = azurerm_user_assigned_identity.cae_identity_selc.client_id
+    principal_id = azurerm_user_assigned_identity.cae_identity_selc.principal_id
+  }
+
+  description = "Details about the user-assigned managed identity created to manage roles of the Container Apps of Selfcare Environment"
+}
+
+output "user_assigned_identity_pnpg" {
+  value = {
+    id           = azurerm_user_assigned_identity.cae_identity_pnpg.id
+    name         = azurerm_user_assigned_identity.cae_identity_pnpg.name
+    client_id    = azurerm_user_assigned_identity.cae_identity_pnpg.client_id
+    principal_id = azurerm_user_assigned_identity.cae_identity_pnpg.principal_id
+  }
+
+  description = "Details about the user-assigned managed identity created to manage roles of the Container Apps of PNPG Environment"
+}
